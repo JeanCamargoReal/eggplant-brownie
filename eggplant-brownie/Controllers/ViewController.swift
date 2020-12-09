d0b6ab7d3089ecbd63fdff81d6d6ab3a61b21bfd
JeanCamargoReal <jeancamargoreal@gmail.com>
2020-12-09 15:05:59 -0300

    Permitindo que o usuário faça  seleções de ingredientes da refeição

--- eggplant-brownie/Controllers/ViewController.swift
+++ eggplant-brownie/Controllers/ViewController.swift
@@ -11,14 +11,50 @@ protocol AdicionaRefeicaoDelegate {
     func add (_ refeicao: Refeicao)
 }
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    // MARK: - Atributos
     
     var delegate: AdicionaRefeicaoDelegate?
+    var itens: [String] = ["Queijo", "Maionese Verde", "Bacon", "Alface", "Tomate", "Hamburguer"]
+    
+    // MARK: - IBOutlets
     
     @IBOutlet var nomeTextField: UITextField?
     @IBOutlet weak var felicidadeTextField: UITextField?
     
-    // Método
+    // MARK: - UITableViewDataSource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itens.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celula = UITableViewCell(style: .default, reuseIdentifier: nil)
+        let linhaDaTabela = indexPath.row
+        let item = itens[linhaDaTabela]
+        celula.textLabel?.text = item
+        
+        return celula
+    }
+    
+    // MARK: - UITableViewDelegate
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let celula = tableView.cellForRow(at: indexPath) else { return }
+        
+        if celula.accessoryType == .none {
+            celula.accessoryType = .checkmark
+        } else {
+            celula.accessoryType = .none
+        }
+        
+        
+    }
+    
+    
+    // MARK: - ABActions
+    
     @IBAction func adicionar(_ sender: UIButton) {
         
         guard let nomeDaRefeicao = nomeTextField?.text else { 
