3b7f7f690668a762df3fcbe9ef29a49f469c9c33
JeanCamargoReal <jeancamargoreal@gmail.com>
2020-12-10 16:28:48 -0300

    Aplicando popViewController na tela de adicionar item, para tela voltar para anterior quando clicar em adicionar

--- eggplant-brownie/Controllers/AdicionarItensViewController.swift
+++ eggplant-brownie/Controllers/AdicionarItensViewController.swift
@@ -8,9 +8,20 @@
 import UIKit
 
 class AdicionarItensViewController: UIViewController {
+    
+    // MARK: - View life cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
+    
+    // MARK: -  IBAction
+    
+    @IBAction func adicionarItem(_ sender: Any) {
+        // navegar para próxima tela: navigationController.push()
+        // navegar para tela anterior: navigationControler.pop()
+        navigationController?.popViewController(animated: true)
+    }
+    
 }
