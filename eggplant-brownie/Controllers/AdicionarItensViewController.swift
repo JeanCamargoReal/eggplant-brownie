ce40a825b90c251b0ffa32276a9c99a949723600
JeanCamargoReal <jeancamargoreal@gmail.com>
2020-12-10 16:11:44 -0300

    Adicionando botao adicionar, via código.

---
+++ eggplant-brownie/Controllers/AdicionarItensViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AdicionarItensViewController.swift
+//  eggplant-brownie
+//
+//  Created by Jean Camargo on 10/12/20.
+//
+
+import UIKit
+
+class AdicionarItensViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
--- eggplant-brownie/Controllers/ViewController.swift
+++ eggplant-brownie/Controllers/ViewController.swift
@@ -31,6 +31,18 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     @IBOutlet var nomeTextField: UITextField?
     @IBOutlet weak var felicidadeTextField: UITextField?
     
+    
+    // MARK: - View life cycle
+    
+    override func viewDidLoad() {
+        let botaoAdicionaItem = UIBarButtonItem(title: "adicionar", style: .plain, target: self, action: #selector(adicionarItem))
+        navigationItem.rightBarButtonItem = botaoAdicionaItem
+    }
+    
+    @objc func adicionarItem() {
+        print("Adicionar novo item na lista")
+    }
+    
     // MARK: - UITableViewDataSource
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
