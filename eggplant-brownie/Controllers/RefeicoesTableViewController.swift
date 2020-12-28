054d1f9b11441add654158e42a9a06ae818f8046
JeanCamargoReal <jeancamargoreal@gmail.com>
2020-12-28 17:12:38 -0300

    Adicionando Gesture Recognizer ->  Long Press

--- eggplant-brownie/Controllers/RefeicoesTableViewController.swift
+++ eggplant-brownie/Controllers/RefeicoesTableViewController.swift
@@ -26,12 +26,30 @@ class RefeicoesTableViewController: UITableViewController, AdicionaRefeicaoDeleg
         let refeicao = refeicoes[indexPath.row]
         celula.textLabel?.text = refeicao.nome
         
+        let longPress = UILongPressGestureRecognizer(target: self, action: #selector(mostrarDetalhes(_:)))
+        
+        celula.addGestureRecognizer(longPress)
+        
         return celula 
     }
     
     func add (_ refeicao: Refeicao){
         refeicoes.append(refeicao)
         tableView.reloadData()
+    }
+    
+    @objc func mostrarDetalhes(_ gesture: UILongPressGestureRecognizer) {
+        if gesture.state == .began {
+            let celula = gesture.view as! UITableViewCell
+            
+            guard let indexPath = tableView.indexPath(for: celula) else {return}
+            
+            let refeicao = refeicoes[indexPath.row]
+            
+            print(refeicao.nome)
+        }
+        
+        
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
